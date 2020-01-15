4ea933c14e12b72adae30c9dbcdddb8a3a33e876
姜希成 <15553598935@163.com>
2020-01-15 18:22:14 +0800

    优化处理流程

--- com/jxch/common/util/js&ts/ResUtil.js
+++ com/jxch/common/util/js&ts/ResUtil.js
@@ -2,22 +2,46 @@ var ResUtil;
 (function (ResUtil) {
     var State;
     (function (State) {
+        State[State["failure"] = -1] = "failure";
+        State[State["default"] = 0] = "default";
+        State[State["finally"] = 1] = "finally";
         State[State["error"] = 100] = "error";
         State[State["success"] = 200] = "success";
+        State[State["invalidToken"] = 10014] = "invalidToken";
     })(State || (State = {}));
-    function process(resProcessor) {
-        try {
-            return resProcessor[State[resProcessor.state]]();
+    function support(resProcessor, stateCode) {
+        return resProcessor[State[stateCode]] !== undefined;
+    }
+    function execute(resProcessor, stateCode) {
+        resProcessor[State[stateCode]]();
+    }
+    function tryExecute(resProcessor, stateCode) {
+        if (support(resProcessor, stateCode)) {
+            execute(resProcessor, stateCode);
+            return true;
         }
-        catch (e) {
-            return new DefaultResProcessor(resProcessor.state, resProcessor.data)[State[resProcessor.state]]();
+        return false;
+    }
+    function process(res, resProcessor, useDefaultProcessor) {
+        if (useDefaultProcessor === void 0) { useDefaultProcessor = true; }
+        if (support(resProcessor, res.state)) {
+            execute(resProcessor, res.state);
         }
+        else {
+            tryExecute(resProcessor, State.default);
+            if (res.state !== State.success) {
+                tryExecute(resProcessor, State.failure);
+            }
+            if (useDefaultProcessor) {
+                process(res, new DefaultResProcessor(res), false);
+            }
+        }
+        tryExecute(resProcessor, State.finally);
     }
     ResUtil.process = process;
     var DefaultResProcessor = /** @class */ (function () {
-        function DefaultResProcessor(state, data) {
-            this.state = state;
-            this.data = data;
+        function DefaultResProcessor(res) {
+            this.res = res;
         }
         DefaultResProcessor.prototype.error = function () {
             this.log();
@@ -25,9 +49,13 @@ var ResUtil;
         DefaultResProcessor.prototype.success = function () {
             this.log();
         };
+        DefaultResProcessor.prototype.failure = function () {
+            console.log(new Date().toLocaleString() + "[" + State[State.failure] + "]");
+            console.log(this.res);
+        };
         DefaultResProcessor.prototype.log = function () {
-            console.log(new Date().toLocaleString() + ":[" + State[this.state] + "] Data on the next line:");
-            console.log(this.data);
+            console.log(new Date().toLocaleString() + " [" + State[this.res.state] + "] " + this.res.message + ". \nData:");
+            console.log(this.res.data);
         };
         return DefaultResProcessor;
     }());
--- com/jxch/common/util/js&ts/ResUtil.ts
+++ com/jxch/common/util/js&ts/ResUtil.ts
@@ -1,33 +1,71 @@
 module ResUtil {
     enum State {
+        failure = -1,
+        default = 0,
+        finally = 1,
         error = 100,
-        success = 200
+        success = 200,
+        invalidToken = 10014,
     }
 
-    export interface ResProcessor<T> {
+    export interface Res<T> {
         readonly state: number;
+        readonly message: String;
         readonly data?: T;
+    }
 
+    export interface ResProcessor {
         success?();
 
         error?();
+
+        failure?();
+
+        default?();
+
+        finally?();
+
+        invalidToken?();
     }
 
-    export function process<T>(resProcessor: ResProcessor<T>) {
-        try {
-            return resProcessor[State[resProcessor.state]]();
-        } catch (e) {
-            return new DefaultResProcessor(resProcessor.state, resProcessor.data)[State[resProcessor.state]]();
+    function support(resProcessor: ResProcessor, stateCode: State) {
+        return resProcessor[State[stateCode]] !== undefined;
+    }
+
+    function execute(resProcessor: ResProcessor, stateCode: State) {
+        resProcessor[State[stateCode]]();
+    }
+
+    function tryExecute(resProcessor: ResProcessor, stateCode: State) {
+        if (support(resProcessor, stateCode)) {
+            execute(resProcessor, stateCode);
+            return true;
         }
+        return false;
     }
 
-    class DefaultResProcessor<T> implements ResProcessor<T> {
-        readonly state: number;
-        readonly data?: T;
+    export function process<T>(res: Res<T>, resProcessor: ResProcessor, useDefaultProcessor: Boolean = true) {
+        if (support(resProcessor, res.state)) {
+            execute(resProcessor, res.state);
+        } else {
+            tryExecute(resProcessor, State.default);
+            if (res.state !== State.success) {
+                tryExecute(resProcessor, State.failure);
+            }
 
-        constructor(state: number, data?: T) {
-            this.state = state;
-            this.data = data;
+            if (useDefaultProcessor){
+                process(res, new DefaultResProcessor(res), false);
+            }
+        }
+
+        tryExecute(resProcessor, State.finally);
+    }
+
+    class DefaultResProcessor<T> implements ResProcessor {
+        readonly res: Res<T>;
+
+        constructor(res: Res<T>) {
+            this.res = res;
         }
 
         error() {
@@ -38,9 +76,14 @@ module ResUtil {
             this.log();
         }
 
+        failure() {
+            console.log(new Date().toLocaleString() + "[" + State[State.failure] + "]");
+            console.log(this.res);
+        }
+
         log() {
-            console.log(new Date().toLocaleString() + ":[" + State[this.state] + "] Data on the next line:");
-            console.log(this.data);
+            console.log(new Date().toLocaleString() + " [" + State[this.res.state] + "] " + this.res.message + ". \nData:");
+            console.log(this.res.data);
         }
     }
 }
